e7387a6441e787079d596d0df242a7d4f56548ac
Nacho <nacholopezamat@gmail.com>
2025-12-15 14:07:01 -0500

    Correccion de paginacion

--- src/collections/characters.ts
+++ src/collections/characters.ts
@@ -14,9 +14,8 @@ export const getCharacter = async(id: string): Promise<Character> =>{
 
 export const getCharacters = async( page?: number, size ?: number): Promise<Character[]> =>{
     const db = getDb()
-    page = page || 1
-    size = size || 3
-
+    if (!page) page = 1
+    if (!size) size = 1
     // la formula es que si quieres 3 objetos de la pagina 1, simplemente obtendra hasta el limit y asi con cualquier ejemplo.
     return await db.collection<Character>(CharacterCollection).find().skip((page - 1) * size).limit(size).toArray();
 }
